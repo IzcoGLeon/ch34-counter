9bf74937d36bfacb4a28dc6ff3da3728e6266a51
IzcoGLeon <Izco.g.leon@outlook.com>
2023-11-28 18:10:58 -0600

    created Counter component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1 className='title'>Counter</h1>
+        <p className='counter-text'>0</p>
+        <button id="sum" type='button'>Sum</button>
+        <button id="substract" type='button'>Subtract</button>
+        <button id="reset" type='button'>Reset</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//agregar componente que tenga valor numerico que se llame valor numerico y va a mostrar un numero y tres botones
